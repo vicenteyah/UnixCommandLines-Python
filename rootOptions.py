1ab033ea941c1e65da76d19afcbeabb155b97ff1
vicente yah <vicente.yah.dzul@icloud.com>
2018-12-29 18:38:50 -0600

    rootOtions file was created

---
+++ rootOptions.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+from time import sleep
+
+def changePermissions():
+    os.system('sudo su')
+    print()
+    file = input("type the file name to change the permissions in DirA: ")
+    os.system('ls -lha {0}'.format("DirA"))
+    os.system('sudo chmod 111 {0}/{1}'.format("DirA", file))
+    sleep(1)
+    os.system('ls -lha {0}\n'.format("DirA"))
+    sleep(2)
+    print(".......Done!")
+
+def changeOwner():
+    
